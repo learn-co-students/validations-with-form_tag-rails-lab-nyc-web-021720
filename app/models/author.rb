71f9361c44dc0d7c562a6295a6cc83a97fcf90a6
codenameuriel <u.e.rodriguez@gmail.com>
2020-03-17 20:41:34 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ActiveRecord::Base
+  # name is not blank
+  # email is unique
+  # phone_number is exactly 10 digits long
+  validates :name, presence: true
+  # error message auto populates the attribute name (Email)
+  validates :email, uniqueness: { is: true, message: "is already taken" }
+  # error message auto populates the attribute name (Phone number)
+  validates :phone_number, length: { is: 10, message: "has to be 10 digits long" } 
 end
